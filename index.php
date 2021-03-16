469085dd4a424bcea97d1a676132697c70356bcf
Antonio <antoniofiorentino16@gmail.com>
2021-03-16 17:31:18 +0100

    Esercizio

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ class Product {
 
     public $category;
 
-    public function __construct($name, $price, $category) {
+    public function __construct(string $name, $price, string $category) {
         $this->name = $name;
         $this->price = $price;
         $this->category = $category;
@@ -29,12 +29,20 @@ class Client {
 
     public $password;
 
-    public function __construct($name, $surname, $email, $password) {
+    public function __construct(string $name, string $surname, string $email, string $password) {
         $this->name = $name;
-        $this->price = $price;
+        $this->surname = $surname;
         $this->email = $email;
         $this->password = $password;
     }
+
+}
+
+class Client extends Payments {
+
+}
+
+class Product extends Payments {
 
 }
 
@@ -43,4 +51,7 @@ class Client {
 $forno = new Product('Forno', '100$', 'Elettrodomestici');
 var_dump($forno);
 
-$
+echo "<br><br>";
+
+$cliente1 = new Client('andrea', 'lamanna', 'anlam@gm.it', 'Lamaan007');
+var_dump($cliente1);
